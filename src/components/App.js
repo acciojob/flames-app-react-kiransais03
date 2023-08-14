af9aaee32131c1993e17a2280942eee1502192f0
kiransai <kiransais03@gmail.com>
2023-08-14 23:27:15 +0530

    submitting code-Mon Aug 14 2023 23:27:12 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,67 @@
-import React, {Component, useState} from "react";
+import React, {useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    render() {
+const App= ()=> {
 
+let [names,setNames] = useState({yname:"",pname:""});
+
+let [clicked,setClicked] =useState(false);
+ 
+  let [rel,setRel]= useState("");
+    function submitted(e) {
+      e.preventDefault();
+      if(names.yname!=="" && names.pname!=="")
+      {
+        console.log("hi");
+        let ynametemparr = names.yname;
+        let pnametemparr = names.pname;
+
+        for(let i=0;i<names.yname.length;i++)
+        {
+            let yname1=ynametemparr.split("");
+            let pname1=pnametemparr.split("");
+            console.log(yname1,"yname",pname1,"pname",names.yname[i],i)
+
+            if(pname1.indexOf(names.yname[i])>=0)
+            {
+                let pind = pname1.indexOf(names.yname[i])
+                console.log(pind,"pind",pname1)
+                yname1.shift();
+                pname1.splice(pind,1)
+                ynametemparr=yname1.join("");
+                pnametemparr=pname1.join("");
+                console.log("tme",yname1,pname1)
+            }
+        }
+        // setNames({...names,yname:ynametemparr,pname:pnametemparr})
+        let length=ynametemparr.length + pnametemparr.length;
+        console.log(ynametemparr,pnametemparr)
+        let ans= length%6;
+        
+        if(ans===0){setRel("Siblings")}
+        else if(ans===1){setRel("Friends")}
+        else if(ans===2){setRel("Love")}
+        else if(ans===3){setRel("Affection")}
+        else if(ans===4){setRel("Marriage")}
+        else if(ans===5){setRel("Enemy")}
+          setClicked(true);
+      }
+    }
+   
         return(
             <div id="main">
                {/* Do not remove the main div */}
+               <form onSubmit={submitted}>
+                <input type="text" data-testid="input1" placeholder="Enter Your Name" value={names.yname} onChange={(e)=>{setNames({...names,yname:e.target.value})}} />
+                 <input type="text" data-testid="input2" placeholder="Enter Your Partner Name" value={names.pname} onChange={(e)=>{setNames({...names,pname:e.target.value})}} />
+                 <button type="submit" data-testid="calculate_relationship">Calculate Relationship Future</button>
+                 <button type="reset" onClick={()=>{setNames({yname:"",pname:""},setClicked(false))}} data-testid="clear">Clear</button>
+               </form>
+               {clicked && <h3 data-testid="answer">{rel}</h3>}
             </div>
         )
     }
-}
+
 
 
 export default App;
